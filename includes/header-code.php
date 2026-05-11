d256282248801e98c4412836f5f50be610c1855d
BP <bp@BPs-MacBook-Air.local>
2026-05-11 14:53:51 +0530

    gsc new

--- includes/header-code.php
+++ includes/header-code.php
@@ -3,8 +3,11 @@
 <link rel="icon" href="/assets/img/logo.png">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+<link
+    href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&family=Roboto:wght@400;500;700&display=swap"
+    rel="stylesheet">
 <link href="/assets/css/style.css" rel="stylesheet">
 
 <meta name="google-site-verification" content="googledf03d7519f6762fc" />
 <script src="https://apis.google.com/js/platform.js"></script>
+<meta name="google-site-verification" content="aNK3SmjAk3DHPbKNXNPK-iidZ4t8zPmP5Kj_P-nGlZw" />
